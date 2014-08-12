002c1f64310e928af273637777e9f9598cffdebf
anders <andersmholmgren@gmail.com>
2014-08-12 19:36:24 +1000

    jwt test

---
+++ test/session/jwt/jwt_session_test.dart
@@ -0,0 +1,41 @@
+library shelf_auth.session.jwt.test;
+
+import 'package:shelf_auth/src/session/jwt/jwt_session.dart';
+import 'package:unittest/unittest.dart';
+import 'package:dart_jwt/dart_jwt.dart';
+
+main() {
+  group('encode decode rountrip', () {
+    var issuer = 'da issuer';
+
+    var subject = 'el subjecto';
+
+    String token() => createSessionToken('secret sauce', issuer, subject);
+    JsonWebToken<SessionClaimSet> roundTrip() => decodeSessionToken(token());
+    SessionClaimSet claimSet() => roundTrip().claimSet;
+
+    test('has matching issuer', () {
+      expect(claimSet().issuer, equals(issuer));
+    });
+
+    test('has matching subject', () {
+      expect(claimSet().subject, equals(subject));
+    });
+
+    test('has expected issuedAt', () {
+      expect(claimSet().issuedAt.millisecondsSinceEpoch,
+        closeTo(new DateTime.now().millisecondsSinceEpoch, 1200));
+    });
+
+    test('has expected expiry offset', () {
+      expect(claimSet().expiry.difference(claimSet().issuedAt),
+        equals(const Duration(minutes: 30)));
+    });
+
+    test('has expected expiry offset', () {
+      expect(claimSet().totalSessionExpiry.difference(claimSet().issuedAt),
+      equals(const Duration(days: 1)));
+    });
+  });
+
+}
