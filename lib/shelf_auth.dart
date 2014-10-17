fba7bbd3e1b25c2a1d1de423e5602626e58cb08a
Anders Holmgren <andersmholmgren@gmail.com>
2014-10-18 04:15:50 +1100

    extra exports

--- lib/shelf_auth.dart
+++ lib/shelf_auth.dart
@@ -7,5 +7,7 @@ library shelf_auth;
 
 export 'src/authentication.dart';
 export 'src/authenticators/basic_auth.dart';
+export 'src/authenticators/core.dart';
 export 'src/authenticators/username_password_auth.dart';
 export 'src/session/jwt/jwt_session_handler.dart';
+export 'src/principal/user_lookup.dart';
